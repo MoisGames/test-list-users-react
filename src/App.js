fc42f8a0282700a21223bbd7c30586b256fb883a
moisgames <VLAD981010@gmail.com>
2024-10-14 20:12:50 +0700

    feat Добавил роутинг, установил SCSS. Сделал боковую панель и блок сортировка. Необходимо добавить кнопки

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import './App.scss';
+import { Routes, Route, Link } from 'react-router-dom';
+import SideBar from './components/SideBar'
 
 function App() {
   return (
+
     <div className="App">
+      <SideBar />
+      <span>
+        APP
+      </span>
+      <Routes>
       
+      </Routes>
     </div>
   );
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SideBar.scss'
+
+const SideBar = () => {
+    return (
+        <div>
+            <main className='sidebar-main'>
+                <span className='sidebar-span'>
+                    Сортировка
+                </span>
+            </main>
+        </div>
+    );
+};
+
+export default SideBar;
